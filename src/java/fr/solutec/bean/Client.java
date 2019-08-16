dbd798acaa9cf2d829e01220a291aeb7323bb3d9
Nicolas_Soliveau <nicolassoliveau@gmail.com>
2019-08-16 10:07:27 +0200

    Client & ClientDao DONE

---
+++ src/java/fr/solutec/bean/Client.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.solutec.bean;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author ESIC
+ */
+public class Client extends User {
+    
+    protected String numClient;
+
+    public Client(String numClient, int id, String nom, String prenom, String email, String tel, Date dateConnexion, String mdp) {
+        super(id, nom, prenom, email, tel, dateConnexion, mdp);
+        this.numClient = numClient;
+    }
+    
+    public Client(){
+        
+    }
+
+    public String getNumClient() {
+        return numClient;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public Date getDateConnexion() {
+        return dateConnexion;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setNumClient(String numClient) {
+        this.numClient = numClient;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setDateConnexion(Date dateConnexion) {
+        this.dateConnexion = dateConnexion;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+    
+}
