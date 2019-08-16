37635018767b37511d11af6099921f9604732f07
esic <esic@DESKTOP-PP0GA40>
2019-08-16 10:32:11 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/java/fr/solutec/bean/Admin.java
+++ src/java/fr/solutec/bean/Admin.java
@@ -5,45 +5,96 @@
  */
 package fr.solutec.bean;
 
+import java.sql.Date;
+
 /**
  *
  * @author esic
  */
-public class Admin {
-    private User user;
-    private String loginAdmin;
+public class Admin extends User {
+
+    protected String loginAdmin;
 
     public Admin() {
     }
 
-    public Admin(User user, String loginAdmin) {
-        this.user = user;
+    public Admin(String loginAdmin, int id, String nom, String prenom, String email, String tel, Date dateConnexion, String mdp) {
+        super(id, nom, prenom, email, tel, dateConnexion, mdp);
         this.loginAdmin = loginAdmin;
     }
 
-    public User getUser() {
-        return user;
+    public Admin(String loginAdmin) {
+        this.loginAdmin = loginAdmin;
     }
 
     public String getLoginAdmin() {
         return loginAdmin;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public Date getDateConnexion() {
+        return dateConnexion;
+    }
+
+    public String getMdp() {
+        return mdp;
     }
 
     public void setLoginAdmin(String loginAdmin) {
         this.loginAdmin = loginAdmin;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setDateConnexion(Date dateConnexion) {
+        this.dateConnexion = dateConnexion;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
     @Override
     public String toString() {
-        return "Admin{" + "user=" + user + ", loginAdmin=" + loginAdmin + '}';
-    } 
-    
+        return "Admin{" + "loginAdmin=" + loginAdmin + '}';
     }
- 
-    
-    
-    
+
+}
