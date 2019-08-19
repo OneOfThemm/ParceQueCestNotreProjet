2ec760c42070e058e8f0056f44bbf06ce0469a94
esic <esic@DESKTOP-PP0GA40>
2019-08-19 10:19:49 +0200

    optimisation des liens inter age admin et servlet

--- src/java/fr/solutec/servlet/AjoutConseillerServlet.java
+++ src/java/fr/solutec/servlet/AjoutConseillerServlet.java
@@ -63,7 +63,8 @@ public class AjoutConseillerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+          request.getRequestDispatcher("/homeadmin").forward(request, response);
     }
 
     /**
